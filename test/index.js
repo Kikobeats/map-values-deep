16067d72c91923fe2a1208515732936197230a55
Daniel Shmuglin <daniels@wix.com>
2019-07-11 10:44:23 +0300

    pass key/index to callback

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ const test = require('ava')
 const map = require('..')
 
 const square = (value, key) => value * value
+const keyPlusValue = (value, key) => key + value
 
 test('transforms simple object', t => {
   t.deepEqual(map({ two: 2, three: 3 }, square), { two: 4, three: 9 })
@@ -25,6 +26,20 @@ test('transforms array with nested objects/arrays', t => {
     { three: 9, four: 16 },
     [25, 36]
   ])
+})
+
+test('passes object key to mapper function', t => {
+  t.deepEqual(
+    map({ key1: 'foo', key2: 'bar' }, keyPlusValue),
+    { key1: 'key1foo', key2: 'key2bar' }
+  )
+})
+
+test('passes array key (index) to mapper function', t => {
+  t.deepEqual(
+    map([0, 2, 4], keyPlusValue),
+    [0, 3, 6]
+  )
 })
 
 test('Do nothing on non plain objects', t => {
